28fa5edbf6f2608cb9d5bd120aa59cdfae73fc2b
rook <spampump@gmail.com>
2023-12-04 19:42:16 +0100

    greedy algorithms

---
+++ GreedyAlgorithms/Easy/ClassPhotos.py
@@ -0,0 +1,64 @@
+"""
+  DESCRIPTION
+
+  It's photo day at the local school, and you're the photographer assigned to   
+  take class photos. The class that you'll be photographing has an even number  
+  of students, and all these students are wearing red or blue shirts. In fact,  
+  exactly half of the class is wearing red shirts, and the other half is wearing
+  blue shirts. You're responsible for arranging the students in two rows before 
+  taking the photo. Each row should contain the same number of the students and 
+  should adhere to the following guidelines:
+
+    All students wearing red shirts must be in the same row.
+    All students wearing blue shirts must be in the same row.
+  
+    Each student in the back row must be strictly taller than the student
+    directly in front of them in the front row.
+
+  You're given two input arrays: one containing the heights of all the students 
+  with red shirts and another one containing the heights of all the students    
+  with blue shirts. These arrays will always have the same length, and each     
+  height will be a positive integer. Write a function that returns whether or   
+  not a class photo that follows the stated guidelines can be taken.
+
+  Note: you can assume that each class has at least 2 students.
+
+
+  Time complexity O(n)
+  Space complexity O(n)
+"""
+import pytest
+
+
+def classPhotos(blueShirtsHeights, redShirtsHeights):
+    blueShirtsHeights.sort()
+    redShirtsHeights.sort()
+    
+    return (all([red>blue for red, blue in zip(redShirtsHeights, blueShirtsHeights)]) or
+            all([blue>red for red, blue in zip(redShirtsHeights, blueShirtsHeights)]))
+
+
+
+# case [blueShirtsHeights, redShirtsHeights, expected]
+cases = [
+    [[6, 9, 2, 4, 5], [5, 8, 1, 3, 4], True],
+    [[5, 8, 1, 3, 4], [6, 9, 2, 4, 5], True],
+    [[5, 8, 1, 3, 4, 9], [6, 9, 2, 4, 5, 1], False],
+    [[6], [6], False],
+    [[125], [126], True],
+    [[2, 3, 4, 5, 6], [1, 2, 3, 4, 5], True],
+    [[5, 6, 7, 2, 3, 1, 2, 3], [1, 1, 1, 1, 1, 1, 1, 1], False],
+    [[2, 2, 2, 2, 2, 2, 2, 2], [1, 1, 1, 1, 1, 1, 1, 1], True],
+    [[126], [125], True],
+    [[21, 5, 4, 4, 4, 4, 4, 4, 4], [19, 2, 4, 6, 2, 3, 1, 1, 4], False],
+    [[20, 5, 4, 4, 4, 4, 4, 4], [19, 19, 21, 1, 1, 1, 1, 1], False],
+    [[2, 4, 7, 5, 1], [3, 5, 6, 8, 2], True],
+    [[2, 4, 7, 5, 1, 6], [3, 5, 6, 8, 2, 1], False],
+    [[5, 4], [4, 5], False],
+    [[5, 4], [5, 6], True]
+]
+
+@pytest.mark.parametrize("blueShirtsHeights, redShirtsHeights, expected", cases)
+def test_classPhotos(blueShirtsHeights, redShirtsHeights, expected):
+    assert classPhotos(blueShirtsHeights, redShirtsHeights)==expected
+
---
+++ GreedyAlgorithms/Easy/MinimumWaitingTime.py
@@ -0,0 +1,66 @@
+"""
+  DESCRIPTION
+
+  You're given a non-empty array of positive integers representing the amounts  
+  of time that specific queries take to execute. Only one query can be executed 
+  at a time, but the queries can be executed in any order.
+
+
+  A query's waiting time is defined as the amount of time that it must
+  wait before its execution starts. In other words, if a query is executed      
+  second, then its waiting time is the duration of the first query; if a query  
+  is executed third, then its waiting time is the sum of the durations of the   
+  first two queries.
+
+
+  Write a function that returns the minimum amount of total waiting time for all
+  of the queries. For example, if you're given the queries of durations
+  [1, 4, 5], then the total waiting time if the queries were
+  executed in the order of [5, 1, 4] would be
+  (0) + (5) + (5 + 1) = 11. The first query of duration
+  5 would be executed immediately, so its waiting time would be
+  0, the second query of duration 1 would have to wait
+  5 seconds (the duration of the first query) to be executed, and
+  the last query would have to wait the duration of the first two queries before
+  being executed.
+
+  Note: you're allowed to mutate the input array.
+
+
+  Time complexity O(###)
+  Space complexity O(###)
+"""
+import pytest
+
+
+def minimumWaitingTime(queries):
+    queries.sort()
+    waiting_time = 0
+    for i in range(1,len(queries)):
+        waiting_time += sum(queries[:i])
+    return waiting_time
+
+
+# case [queries, expected]
+cases = [
+    [[3, 2, 1, 2, 6], 17],
+    [[2, 1, 1, 1], 6],
+    [[1, 2, 4, 5, 2, 1], 23],
+    [[25, 30, 2, 1], 32],
+    [[1, 1, 1, 1, 1], 10],
+    [[7, 9, 2, 3], 19],
+    [[4, 3, 1, 1, 3, 2, 1, 8], 45],
+    [[2], 0],
+    [[7], 0],
+    [[8, 9], 8],
+    [[1, 9], 1],
+    [[5, 4, 3, 2, 1], 20],
+    [[1, 2, 3, 4, 5], 20],
+    [[1, 1, 1, 4, 5, 6, 8, 1, 1, 2, 1], 81],
+    [[17, 4, 3], 10]
+]
+
+@pytest.mark.parametrize("queries, expected", cases)
+def test_minimumWaitingTime(queries, expected):
+    assert minimumWaitingTime(queries)==expected
+
